78dc10b3a334106facb1b95f7a17ccdd8082c746
Diego <diegochowguer@gmail.com>
2015-09-28 17:15:24 -0300

    28/09/2015-17:15

--- app/src/main/java/br/com/ohexpress/ohex/MainActivity.java
+++ app/src/main/java/br/com/ohexpress/ohex/MainActivity.java
@@ -101,6 +101,7 @@ public class MainActivity extends ActionBarActivity {
 
 
         ohTopBar = (Toolbar) findViewById(R.id.oh_top_toolbar);
+        ohTopBar.setTitle("");
         setSupportActionBar(ohTopBar);
 
 
